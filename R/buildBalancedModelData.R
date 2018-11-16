a8acc895db8a76960d18d762f5acba5d8a975575
adamkc <adamkc@gmail.com>
2018-11-16 11:28:59 -0800

    merged buildBalancedModelData into buildModelData. Latter unneeded now.

--- R/buildBalancedModelData.R
+++ R/buildBalancedModelData.R
@@ -6,10 +6,10 @@
 #' class dictates how much training data is added for each class.  The remainders are added to
 #' the validation dataset.
 #'
-#' @param data.folder
-#' @param trainpercent
-#' @param validatepercent
-#' @param testpercent
+#' @param data.folder Top directory that contains "Original" folder. "Model Data" will be added to this directory as well.
+#' @param trainpercent Percent of data to add to the training dataset. These three need to add to 1.
+#' @param validatepercent Percent of data to add to the validation dataset. These three need to add to 1.
+#' @param testpercent Percent of data to add to the test dataset. These three need to add to 1.
 #'
 #' @return
 #' @export
@@ -33,7 +33,8 @@ buildBalancedModelData <- function(data.folder,
   sapply(classlist,function(x) dir.create(paste0("Model Data/training/",x)))
   sapply(classlist,function(x) dir.create(paste0("Model Data/Validation/",x)))
   labelandmoveoneclass <- function(targetclass = classlist[4]){
-    classImages <- list.files(paste0(data.folder,"/Original/",targetclass))
+    classFolder <- paste0(data.folder,"/Original/",targetclass)
+    classImages <- list.files(classFolder)
     trainingImages <- sample(classImages,size = round(minClass*trainpercent,0),replace = FALSE)
     valPool <- classImages[!(classImages %in% trainingImages)]
     validationImages <- sample(valPool,size = round(minClass*validatepercent,0),replace = FALSE)
@@ -41,10 +42,10 @@ buildBalancedModelData <- function(data.folder,
     testImages <- sample(testPool,size=round(minClass*testpercent,0),replace = FALSE)
     testExtraImages <- testPool[!(testPool %in% testImages)]
 
-    file.copy(paste0(data.folder,"/Original/",targetclass,"/",trainingImages),paste0(data.folder,"/Model Data/training/",targetclass))
-    file.copy(paste0(data.folder,"/Original/",targetclass,"/",validationImages),paste0(data.folder,"/Model Data/validation/",targetclass))
-    file.copy(paste0(data.folder,"/Original/",targetclass,"/",testImages),paste0(data.folder,"/Model Data/testing/Unclassified"))
-    file.copy(paste0(data.folder,"/Original/",targetclass,"/",testExtraImages),paste0(data.folder,"/Model Data/testingExtra/Unclassified"))
+    file.copy(paste0(classFolder,"/",trainingImages),paste0(data.folder,"/Model Data/training/",targetclass))
+    file.copy(paste0(classFolder,"/",validationImages),paste0(data.folder,"/Model Data/validation/",targetclass))
+    file.copy(paste0(classFolder,"/",testImages),paste0(data.folder,"/Model Data/testing/Unclassified"))
+    file.copy(paste0(classFolder,"/",testExtraImages),paste0(data.folder,"/Model Data/testingExtra/Unclassified"))
     print(paste0(targetclass, " copying is complete with ",length(trainingImages),
                  " training images and ",length(testImages), " test images"))
   }
--- R/buildModelData.R
+++ R/buildModelData.R
@@ -2,10 +2,10 @@
 #'
 #' @description Create train, validation and training data from a pool of original class sorted data
 #' This function creates a "Model Data" folder and necessary subfolders with and populated them with
-#' @param data.folder
-#' @param trainpercent
-#' @param validatepercent
-#' @param testpercent
+#' @param data.folder Top directory that contains "Original" folder. "Model Data" will be added to this directory as well.
+#' @param trainpercent Percent of data to add to the training dataset. These three need to add to 1.
+#' @param validatepercent Percent of data to add to the validation dataset. These three need to add to 1.
+#' @param testpercent Percent of data to add to the test dataset. These three need to add to 1.
 #'
 #' @return
 #' @export
@@ -18,38 +18,49 @@
 buildModelData <- function(data.folder = getwd(),
                            trainpercent = 0.85,
                            validatepercent = 0.15,
-                           testpercent = 0.00){
+                           testpercent = 0.00,
+                           balanceData = FALSE){
 
-  if((trainpercent + validationpercent + testpercent) != 1) return("Error: numbers don't add to 1")
+  if((trainpercent + validationpercent + testpercent) != 1)
+    return("Error: numbers don't add to 1")
 
-  oldwd <- getwd()
-  setwd(data.folder)
-
+  #Generate folders:
   sapply(c("Model Data","Model Data/training",
            "Model Data/validation","Model Data/testing","Model Data/testingexport",
-           "Model Data/testing/Unclassified"),dir.create)
-  classlist <- list.files(path = paste0(getwd(), "/Original"))
+           "Model Data/testing/Unclassified","Model Data/testingExtra/Unclassified")
+         ,dir.create)
+
+
+  classlist <- list.files(path = paste0(data.folder, "/Original"))
   minClass <- min(sapply(paste0(data.folder, "/Original/",classlist),function(x) length(list.files(x))))
   valSize <- round(minClass * validatepercent)
   sapply(classlist,function(x) dir.create(paste0("Model Data/training/",x)))
   sapply(classlist,function(x) dir.create(paste0("Model Data/Validation/",x)))
-  labelandmoveoneclass <- function(targetclass = classlist[1]){
-    classImages <- list.files(paste0(getwd(),"/Original/",targetclass))
-    trainingImages <- sample(classImages,size = round(length(classImages)*trainpercent,0),replace = FALSE)
+
+  labelAndMoveOneClass <- function(targetclass = classlist[1]){
+    classFolder <- paste0(data.folder,"/Original/",targetclass)
+    classImages <- list.files(classFolder)
+    classSize <- length(classImages)
+
+    if(balanceData)
+      classSize <- minClass
+
+    trainingImages <- sample(classImages,size = round(classSize*trainpercent,0),replace = FALSE)
     valPool <- classImages[!(classImages %in% trainingImages)]
     validationImages <- sample(valPool,size = valSize,replace = FALSE)
-    testImages <- valPool[!(valPool %in% validationImages)]
-    file.copy(paste0(getwd(),"/Original/",targetclass,"/",trainingImages),paste0(getwd(),"/Model Data/training/",targetclass))
-    file.copy(paste0(getwd(),"/Original/",targetclass,"/",validationImages),paste0(getwd(),"/Model Data/validation/",targetclass))
-    file.copy(paste0(getwd(),"/Original/",targetclass,"/",testImages),paste0(getwd(),"/Model Data/testing/Unclassified"))
+    testPool <- valPool[!(valPool %in% validationImages)]
+    testImages <- sample(testPool,size=round(classSize*testpercent,0),replace = FALSE)
+    testExtraImages <- testPool[!(testPool %in% testImages)]
+
+    file.copy(paste0(classFolder,"/",trainingImages),paste0(data.folder,"/Model Data/training/",targetclass))
+    file.copy(paste0(classFolder,"/",validationImages),paste0(data.folder,"/Model Data/validation/",targetclass))
+    file.copy(paste0(classFolder,"/",testImages),paste0(data.folder,"/Model Data/testing/Unclassified"))
+    file.copy(paste0(classFolder,"/",testExtraImages),paste0(data.folder,"/Model Data/testingExtra/Unclassified"))
 
     print(paste0(targetclass, " copying is complete with ",
                  length(trainingImages)," training images and ",
                  length(validationImages)," validation images and ",
                  length(testImages), " test images"))
   }
-  sapply(classlist,labelandmoveoneclass)
-  file.rename(paste0(getwd(),"Model Data"),
-              paste0(getwd(),"Model Data/",Sys.Date()))
-  setwd(oldwd)
+  sapply(classlist,labelAndMoveOneClass)
 }
